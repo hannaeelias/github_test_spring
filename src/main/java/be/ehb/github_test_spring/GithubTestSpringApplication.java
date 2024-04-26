f7ca144eacdba1a4de5e9445a3878243300d9194
hannae elias <hannae.elias@outlook.fr>
2024-04-26 14:36:09 +0200

    SET UP

---
+++ src/main/java/be/ehb/github_test_spring/GithubTestSpringApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.github_test_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTestSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/github_test_spring/GithubTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.github_test_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
